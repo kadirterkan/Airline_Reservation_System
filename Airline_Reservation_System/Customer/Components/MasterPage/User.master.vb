eea6d418284cd0545402bdcd05d27c52135b9802
kadir <kadirtayyiperkan@hotmail.com>
2022-01-16 16:55:10 +0300

    master
    Master commit.

--- Airline_Reservation_System/Customer/Components/MasterPage/User.master.vb
+++
@@ -1,5 +0,0 @@
-﻿
-Partial Class Customer_Components_MasterPage_User
-    Inherits System.Web.UI.MasterPage
-End Class
-
--- Airline_Reservation_System/Customer/Pages/HomePage/HomePage.aspx.vb
+++
@@ -1,5 +0,0 @@
-﻿
-Partial Class Customer_Pages_HomePage_HomePage
-    Inherits System.Web.UI.Page
-
-End Class
