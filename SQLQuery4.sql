5727a9814267460659a6221c063c32ef5db81387
kadir <kadirtayyiperkan@hotmail.com>
2022-01-21 10:05:50 +0300

    README.md ekleyin.

---
+++ SQLQuery4.sql
@@ -0,0 +1,15 @@
+﻿SET IDENTITY_INSERT Authority ON;
+
+INSERT INTO Authority(ID,Creation_Date, Update_Date, User_Type_Id, Authority_Name)
+VALUES(1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 2, 'ADD_FLIGHT');
+
+
+INSERT INTO Authority(ID,Creation_Date, Update_Date, User_Type_Id, Authority_Name)
+VALUES(1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 2, 'ADD_ROUTE');
+
+
+INSERT INTO Authority(ID,Creation_Date, Update_Date, User_Type_Id, Authority_Name)
+VALUES(1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 2, '');
+
+INSERT INTO Authority(ID,Creation_Date, Update_Date, User_Type_Id, Authority_Name)
+VALUES(1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, 'BOOK_FLIGHT');
